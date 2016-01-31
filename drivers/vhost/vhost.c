91622c4e32bf33bb380c5772de8357fa8f38f7fd
Yossi Kuperman <yossiku@il.ibm.com>
2016-01-31 20:54:10 +0200

    fix a bug where a variable wasn't initialized
    Change-Id: Iea0936909806eed5592d5c10bb664302ed28c4ea

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -563,6 +563,7 @@ void vhost_poll_init(struct vhost_poll *poll, vhost_work_fn_t fn,
 	init_poll_funcptr(&poll->table, vhost_poll_func);
 	poll->mask = mask;
 	poll->dev = vq->dev;
+	poll->wqh = NULL;
 
 	vhost_work_init(&poll->work, vq, fn);
 }
